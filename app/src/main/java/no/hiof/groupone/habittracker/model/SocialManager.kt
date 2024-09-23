77c364e260b596badb66665418b31f3e23136e55
steffenhj <xrevdoe@hotmail.com>
2024-09-23 21:50:58 +0200

    Outline of Social functions (to be added)

---
+++ app/src/main/java/no/hiof/groupone/habittracker/model/SocialManager.kt
@@ -0,0 +1,27 @@
+package no.hiof.groupone.habittracker.model
+
+class SocialManager {
+
+    fun addFriend(userId: String , friendId: String) {
+
+    }
+
+    fun removeFriend(userId: String , friendId: String) {
+
+    }
+
+    fun inviteToHabit(userId: String , habitId: String) {
+
+    }
+
+    fun getFriends(userId: String) : List<String> {
+        return listOf()
+    }
+
+    fun isFriend(userId: String , friendId: String) : Boolean {
+        return true
+    }
+
+
+
+}
