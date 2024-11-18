bdbb3284a1a60fc05de34394505c5f9d27c545e3
Alexander K <alexander.karlsen87@gmail.com>
2024-11-18 16:42:02 +0100

    Removed deprecated function.

--- app/src/main/java/no/hiof/groupone/habittracker/MyApplication.kt
+++ app/src/main/java/no/hiof/groupone/habittracker/MyApplication.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.firestore.PersistentCacheSettings
 import com.jakewharton.threetenabp.AndroidThreeTen
 
 class MyApplication : Application() {
@@ -15,10 +16,15 @@ class MyApplication : Application() {
         super.onCreate()
         AndroidThreeTen.init(this)
 
-        FirebaseFirestore.getInstance().firestoreSettings = FirebaseFirestoreSettings.Builder()
-            .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
-            .setPersistenceEnabled(true)
+        val persistentCacheSettings = PersistentCacheSettings.newBuilder()
+            .setSizeBytes(-1)
             .build()
+
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setLocalCacheSettings(persistentCacheSettings)
+            .build()
+
+        FirebaseFirestore.getInstance().firestoreSettings = settings
 
         val notificationChannel = NotificationChannel(
             "notification_channel_id",
